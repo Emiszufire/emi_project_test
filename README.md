271edd1e3adadaadc3080d5b0563bcada66b0a75
EmiSzu <emiszu@gmail.com>
2025-05-05 22:25:49 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# emi_project_test
+# Project Playwright, Pytest
+
+## REPO
+1. Download/Clone repo
+
+## PYTHON & PYCHARM
+1. configure VENV
+2. Install Pycharm (optional AI Assistant) & Python
+### Install python modules
+```bash
+python.exe -m pip install --upgrade pip
+```
+```bash
+pip install -r requirements.txt
+```
+
+## PLAYWRIGHT
+```bash
+playwright install
+```
---
+++ test_1sample.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+@pytest.fixture
+def one():
+    return 1
+
+@pytest.fixture
+def two():
+    return 3
+
+def test_pass(one):
+    actual = one
+    expected = 1
+    assert actual == expected, "Actual value does not match expected value"
+
+def test_fail(two):
+    actual = two
+    expected = 1
+    assert actual == expected, "Actual value does not match expected value"
